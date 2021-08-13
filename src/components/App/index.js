e9b0ffb9f727d97d92fe09b48457834dddfd9439
DUONG <c-dng>
2021-08-14 00:54:52 +0200

    installation de semantic-ui-react semantic-ui-css

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+// == Import npm
+import React from 'react';
+
+import './app.scss';
+
+
+
+// == Composant
+const App = () => { 
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+// == Export
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
+import App from './components/App';
 
 // == Render
 // 1. Élément React racine (celui qui contient l'ensemble de l'app)
