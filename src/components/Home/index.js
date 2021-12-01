7a4f2fd8e275b4584e98255ea6d32847df908021
Camille Duong <camille.duong@gmail.com>
2021-12-01 17:33:14 +0100

    edit intro component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,8 +7,8 @@ const Home = () => (
   <div className="home">
      <div className="home-mainWrapper">
           <Image className="home_photo" src={photo} size='small' circular />
-          <Header className="home_titleHeader">Camille Duong</Header>
-            <div className="home_description">Développeuse Web React</div>
+          <Header className="home_titleHeader"> Développeuse Web React </Header>
+            <div className="home_description"> Camille Duong </div>
     </div>
   </div>
 );
--- src/components/Intro/index.js
+++ src/components/Intro/index.js
@@ -1,13 +1,25 @@
 import React from 'react'
-// import { Icon, Radio } from 'semantic-ui-react'
+import { Container, Header } from 'semantic-ui-react'
 import './intro.scss';
 
 const Intro = () => (
-    <div className="intro">
-        <h1>Présentation</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-    </div>
+  <div className="intro">
+    <Header size="{large}" as='h2' icon textAlign='center'>
+      <Header.Content>PRÉSENTATION</Header.Content>
+    </Header>
+    <Container text>
+            <p>
+                Bonjour, je suis une développeuse web front-end spécialisée React.
+            </p>
+            <p>
+                Issue d'une reconversion professionnelle, j'ai entammé une formation de développeur web full stack de 5 mois avec l'école O'Clock, ensuite j'ai poursuivi mon apprentissage par une formation sur Python.
+            </p>
+            <p>
+                Je continue à apprendre travaillant sur des projets personnels pour améliorer mes compétences.
+            </p>
+    </Container>
+  </div>
 );
 
-
 export default Intro;
+
